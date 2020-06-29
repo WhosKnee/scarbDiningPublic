dc885c56969a506f94daee2b271ff2b6e8f29c90
Ahmad Shah <ahmadali.shah@mail.utoronto.ca>
2020-06-29 14:42:19 -0400

    (2 T2 P2) Link submitted form to database
    - Added boilerplate for mongoose
    - Added POST method to add new restaurant to database
    - Added docs folder and documentation for new POST method

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 
 // use bodyparser when fetching input data from a form
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // connect mongoose to a local database
 mongoose.connect('mongodb://localhost:27017/scarb_dining', { useNewUrlParser: true });
@@ -23,10 +24,39 @@ var Restaurant = require("./models/restaurant.js");
 var routes = require("./routes/routes.js");
 app.use(routes);
 
-// run app locally on server
-app.listen(3000, 'localhost', function(){
-    console.log("The Notepad server has started on port 3000");
-})
+
+// connecting to db
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("('we're in)");
+
+    // add restaurant to db
+    app.post('/api/restaurants/', function (req, res, next) {
+        let newRestaurant = new Restaurant ({
+            _id: mongoose.Types.ObjectId(),
+            name: req.body.name,
+            phone: req.body.phoneNumber,
+            address: req.body.address,
+            ownerFirstName: req.body.ownerFirstName,
+            ownerLastName: req.body.ownerLastName,
+            ownerTitle: req.body.ownerTitle,
+            ownerEmail: req.body.ownerEmail,
+            ownerPhone: req.body.ownerPhoneNumber
+        });
+
+        newRestaurant.save(function (err, result) {
+            if (err) console.log('error');
+            return res.json(result);
+        });
+    });
+
+    // run app locally on server
+    app.listen(3000, 'localhost', function(){
+        console.log("The Notepad server has started on port 3000");
+    })
+});
+
 
 // to start the server, run 'node app.js' and go to localhost:3000 on
 // any browser
---
+++ docs/DOCS.md
@@ -0,0 +1,35 @@
+# Scarborough Dining REST API Documentation
+
+## API
+### Create
+
+- description: Create a new restaurant
+- request: `POST /api/restaurants/`
+    - content-type: `application/json`
+    - body: object
+      - name: (string) the restaurant name
+      - phoneNumber: (string) the phone number for the restaurant
+      - address: (string) the address of the restaurant
+      - ownerFirstName: (string) the first name of the owner
+      - ownerLastName: (string) the last name of the owner
+      - ownerTitle: (string) the title of the owner
+      - ownerEmail: (string) the email of the owner
+      - ownerPhoneNumber: (string) owner's personal phone
+- response: 200
+    - content-type: `application/json`
+    - body: object
+      - name: (string) the restaurant name
+      - phoneNumber: (string) the phone number for the restaurant
+      - address: (string) the address of the restaurant
+      - ownerFirstName: (string) the first name of the owner
+      - ownerLastName: (string) the last name of the owner
+      - ownerTitle: (string) the title of the owner
+      - ownerEmail: (string) the email of the owner
+      - ownerPhoneNumber: (string) owner's personal phone
+
+``` 
+    $ curl -X POST 
+       -H "Content-Type: `application/json`" 
+       -d {"name": "test restaurant", "phoneNumber": "416 571 2723", "address": "123 Nowhere Road", "ownerFirstName": "John", "ownerLastName": "Smith", "ownerTitle": "Mr.", "ownerEmail": "johnsmith@gmail.com", "ownerPhoneNumber": "647 571 4269"}
+       http://localhost:3000/api/restaurants/'
+```
